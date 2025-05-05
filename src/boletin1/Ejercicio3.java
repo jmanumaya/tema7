579b6f40fa5c73428b1dcea9a2c666cd1ac3e2e6
jmanumaya <jm.maya@iesnervion.es>
2025-05-06 00:04:21 +0200

    Terminado

---
+++ src/boletin1/Ejercicio3.java
@@ -0,0 +1,37 @@
+package boletin1;
+
+import java.sql.*;
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String url = "jdbc:mysql://localhost/InstitutoDB";
+		String usuario = "root";
+		
+		String pass = "";
+		
+		System.out.print(usuario + " Introduce su Contraseña: ");
+		pass = sc.next();
+
+		try (Connection con = DriverManager.getConnection(url, usuario, pass)){
+			System.out.println("Conexión Realizada con Éxito");
+			
+			Statement sent = con.createStatement();
+			
+			// Pruebas de DELETES... (Desde Matriculas antes porque hay restricciones)
+			
+		//	System.out.println(sent.executeUpdate("DELETE from Matriculas where id_matricula = 5"));
+		//	System.out.println(sent.executeUpdate("DELETE from Estudiantes where id_estudiante = 4"));
+		//	System.out.println(sent.executeUpdate("DELETE from Profesores where id_profesor = 3"));
+		//	System.out.println(sent.executeUpdate("DELETE from Cursos where id_curso = 3"));
+				
+		} catch (SQLException e) {
+			System.out.println(e);
+		}
+	}
+
+}
